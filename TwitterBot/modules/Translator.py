edbe80ba10fef6baf4bd27e6b386d7e94dbe5da3
Ichinose Shogo <ichinose@stn.nagaokaut.ac.jp>
2012-02-24 22:08:09 +0900

    翻訳時にタイムスタンプを消去

--- TwitterBot/modules/Translator.py
+++ TwitterBot/modules/Translator.py
@@ -24,18 +24,27 @@ class Translator(object):
         self._re_translate_text = re.compile(ur'^(.*)を(%s)(訳|語訳|語翻訳)' % lang, re.IGNORECASE)
         self._re_translate = re.compile(ur'(%s)(訳|語訳|語翻訳)' % lang, re.IGNORECASE)
 
+    _re_timestamp = re.compile(ur'\[(.*?)\]')
+    def rm_timestamp(self, text):
+        return self._re_timestamp.sub(u'', text)
+
     def hook(self, bot, status):
         m = self._re_translate_text.search(status.text)
         if m:
-            text = m.group(1)
             lang = m.group(2)
+            lang = self.translator.lang2code(lang)
+            text = m.group(1)
+            text = self.rm_timestamp(text)
             text = self._re_mention.sub('', text)
-            bot.reply_to(self.translator.translate(text, None, self.translator.lang2code(lang)), status)
+            text = self.translator.translate(text, None, lang)
+            text = u'[%s語訳]%s [%s]' % (self.translator.code2lang(lang), text, bot.get_timestamp())
+            bot.reply_to(text, status)
             return True
 
         m = self._re_translate.search(status.text)
         if m:
             lang = m.group(1)
+            lang = self.translator.lang2code(lang)
             text = u'翻訳する文章を教えてください'
             m = self._re_retweet.search(status.text)
             if m:
@@ -45,11 +54,11 @@ class Translator(object):
                 #リプライを飛ばした先の文章を翻訳
                 reply_to_status = bot.api.get_status(status.in_reply_to_status_id)
                 text = reply_to_status.text
+            text = self.rm_timestamp(text)
             text = self._re_mention.sub('', text)
-            bot.reply_to(u'[%s語訳]%s [%s]' % (
-                    self.translator._inverse_lang_dict.get(lang, lang),
-                    self.translator.translate(text, None, self.translator._lang_dict.get(lang, lang)),
-                    bot.get_timestamp()), status)
+            text = self.translator.translate(text, None, lang)
+            text = u'[%s語訳]%s [%s]' % (self.translator.code2lang(lang), text, bot.get_timestamp())
+            bot.reply_to(text, status)
             return True
 
         m = self._re_detect.search(status.text)
@@ -63,6 +72,7 @@ class Translator(object):
                 #リプライを飛ばした先の文章を翻訳
                 reply_to_status = bot.api.get_status(status.in_reply_to_status_id)
                 text = reply_to_status.text
+            text = self.rm_timestamp(text)
             text = self._re_mention.sub('', text)
             lang = self.translator.detect(text)
             if lang:
