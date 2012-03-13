65a3b5ea1d4a672335a40fff60b5e04c2c007549
Ichinose Shogo <shogo82148@gmail.com>
2012-03-13 15:33:12 +0900

    tsv2db.pyがリファクタリングに追従していなかったので修正

--- scripts/tsv2db.py
+++ scripts/tsv2db.py
@@ -4,7 +4,7 @@
 import sys
 import codecs
 
-from dbmanager import DBManager
+from TwitteBot.modules.CloneBot.dbmanager import DBManager
 sys.stdin  = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
